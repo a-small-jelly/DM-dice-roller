fe7f0ba15927b09c2287a1c92ca3119ed1364ead
a-small-jelly <66644837+a-small-jelly@users.noreply.github.com>
2021-09-09 12:55:39 -0400

    changed id numbering

--- dmdice.js
+++ dmdice.js
@@ -19,13 +19,12 @@ const d100 = 100;
 
 let counter = 0;
 
-// need to add check for ids and not duplicate!!
 
 function addPC() {
 	// counter
 	counter++;
 	// create variable that is clone of 'pc'
-	let newPCs = document.getElementById('pc').cloneNode(true);
+	let newPCs = document.getElementById('pc-').cloneNode(true);
 	// remove id of clone
 	newPCs.id = '';
 	// display clone
@@ -39,17 +38,11 @@ function addPC() {
 				// change id of childnotes
 				newPC[l].id = theName + counter;
 				// change id of pc div
-				newPCs.id = 'pc' + counter;
+				newPCs.id = 'pc-' + counter;
 	}
-	let insertHere = document.getElementById('additional-space');
+	let insertHere = document.getElementById('additionalSpace');
 	insertHere.parentNode.insertBefore(newPCs, insertHere);
 }
-
-
-// need to stop PCs being added after 9 lol
-// and also fix id numbers
-
-
 
 
 
@@ -70,20 +63,32 @@ function rollDice(die) {
 
 		// check for checkboxes (ik it is redundant it's just for me)
 
-	const ckProf = document.getElementById("applyProf" + i).checked;
-	const ckPer = document.getElementById("applyPer" + i).checked;
+	const ckProf = document.getElementById("applyProf-" + i).checked;
+	const ckPer = document.getElementById("applyPer-" + i).checked;
 
 		// get mods etc and add everything
 
-	const mod = (ckPer == true) ? document.getElementById("perceptionMod" + i).value : 0;
-	const profB = (ckProf == true) ? document.getElementById("profBonus" + i).value : 0;
+	const mod = (ckPer == true) ? document.getElementById("perceptionMod-" + i).value : 0;
+	const profB = (ckProf == true) ? document.getElementById("profBonus-" + i).value : 0;
 
 		// results
 
-	document.getElementById("dieRoll" + i).innerHTML = roll;
-	document.getElementById("prof" + i).innerHTML = profB;
-	document.getElementById("perMod" + i).innerHTML = mod;
-	document.getElementById("total" + i).innerHTML = +roll + +mod + +profB;
+	document.getElementById("dieRoll-" + i).innerHTML = roll;
+	document.getElementById("profBonus-" + i).innerHTML = profB;
+	document.getElementById("perMod-" + i).innerHTML = mod;
+	document.getElementById("total-" + i).innerHTML = +roll + +mod + +profB
+
+		// crit
+	if (roll == "1") {
+	document.getElementById("message-" + i).innerHTML = "critical miss";
+		} else if (roll == "20") {
+	document.getElementById("message-" + i).innerHTML = "critical hit";
+		} else {
+	document.getElementById("message-" + i).innerHTML = null;
+		}
+
+
+	;
 	return false; // 
 
 }
@@ -91,15 +96,18 @@ function rollDice(die) {
 
 function removePC() {
 	const thisButton = event.srcElement.id;
-	const k = thisButton.slice(-1);
-	let deadPC = document.getElementById("pc" + k);
-	console.log(deadPC.id);
+	console.log(thisButton);
+	const k = thisButton.split("-")[1];
+	let deadPC = document.getElementById("pc-" + k);
 	deadPC.remove();
-	counter--;
 	return false;
-	consloe.log(counter);
 
 }
+
+
+
+
+
 
 
 
