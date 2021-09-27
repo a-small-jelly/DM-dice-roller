f513c5c899943218bc4ac20c4f090330ab6cf55a
a-small-jelly <66644837+a-small-jelly@users.noreply.github.com>
2021-09-27 12:26:54 -0400

    Add files via upload
    - history item height determined by pc-wrapper

--- dmdice.js
+++ dmdice.js
@@ -1,6 +1,5 @@
-// NEXT: save for server session
 
-// CURRENTLY: working on roll history
+
 
 
 // dice
@@ -22,8 +21,11 @@ let counter = 0;
 
 
 function addPC() {
-	// counter
 	counter++;
+
+	//default name 
+	document.getElementById("pcName-").value = "PC" + counter;
+
 	// create variable that is clone of 'pc'
 	let newPCs = document.getElementById('pc-').cloneNode(true);
 	// remove id of clone
@@ -33,24 +35,27 @@ function addPC() {
 	// create variable that is childnodes of clone
 	let newPC = newPCs.childNodes;
 	console.log(newPC);
+
 	for (let l = 0; l < newPC.length; l++) {
 		let theName = newPC[l].id
 		if (theName)
-				// change id of childnotes
+				// change id of childnodes
 				newPC[l].id = theName + counter;
 				// change id of pc div
 				newPCs.id = 'pc-' + counter;
 	}
 	let insertHere = document.getElementById('additionalSpace');
 	insertHere.parentNode.insertBefore(newPCs, insertHere);
+
+
 }
 
 
-function rollPlain(die) {
-
+function rollPlain() {
+	const die = document.getElementById("dice").value;
 	const roll = 1 + Math.floor(Math.random()*die);
 	document.getElementById("rollOne").innerHTML = roll;
-	document.getElementById("rollHistory").insertAdjacentHTML('afterbegin', "d20: " + roll + "<br>" )
+	document.getElementById("rollHistory").insertAdjacentHTML('afterbegin', "d" + die + ": " +  roll + "<br>" )
 }
 
 
